71c332a11ac4f5ceb714d0af5088c0332adc63dd
Gökçe Uludoğan <gokceuludogan@gmail.com>
2025-03-03 23:05:03 +0300

    Create example.c

---
+++ src/example.c
@@ -0,0 +1,14 @@
+/**
+ * @file example.c
+ * @brief This is an example function.
+ */
+
+/**
+ * @brief Adds two numbers.
+ * @param a First number.
+ * @param b Second number.
+ * @return Sum of a and b.
+ */
+int add(int a, int b) {
+    return a + b;
+}
